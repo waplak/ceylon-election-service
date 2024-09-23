eaed15b3efa3d20ba6d8227741d75b3d06616374
waplak <pamitha99@gmail.com>
2024-09-23 10:44:38 +1000

    Initiate ceylon election service

---
+++ src/main/java/io/ceylon/election/CeylonElectionServiceApplication.java
@@ -0,0 +1,13 @@
+package io.ceylon.election;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeylonElectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeylonElectionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ceylon/election/CeylonElectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ceylon.election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeylonElectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
